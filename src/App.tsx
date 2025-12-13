6d43d44f0375ae60b4fd784dc4bc11c017709ab5
yuan <yuan0x00@gmail.com>
2025-12-13 14:22:30 +0800

    ToDoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [taskList, setTaskList] = useState<string[]>([]);
+    const [task, setTask] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function addTaskToList(taskText: string) {
+        if (!taskText.trim()) return;
+        setTaskList(prev => [...prev, taskText]);
+    }
+
+    return (
+        <>
+            <div>
+                <input value={task}
+                       onChange={e => setTask(e.target.value)}/>
+                <button onClick={() => addTaskToList(task)}>
+                    添加
+                </button>
+                {taskList.map((task) => (
+                    <div key={task}>{task}</div>
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default App

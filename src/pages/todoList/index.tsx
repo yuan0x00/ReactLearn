ea58a8388499bfd2baa649446605969c1bdbf7fa
yuan <yuan0x00@gmail.com>
2025-12-14 17:50:23 +0800

    1.每次修改input框都需要发一个请求（模拟） 2.记录发请求的总数量，不需要展示到页面上 3.获取todolist的dom节点

--- src/pages/todoList/index.tsx
+++ src/pages/todoList/index.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import TodoTaskInput from './components/TodoTaskInput'
 import TodoTaskAddButton from './components/TodoTaskAddButton'
 import TodoTaskList from './components/TodoTaskList'
@@ -6,21 +6,42 @@ import TodoTaskList from './components/TodoTaskList'
 function TodoList() {
     const [taskList, setTaskList] = useState<string[]>([]);
     const [task, setTask] = useState('');
+    const [requestList, setRequestList] = useState<string[]>([]);
+    const taskListRef = useRef(null);
 
-    function addTaskToList(taskText: string) {
-        if (!taskText.trim()) return;
-        setTaskList(prev => [...prev, taskText]);
-    }
+    useEffect(() => {
+        if (task || !task.trim()) {
+            setRequestList(prev => [...prev, task])
+            console.log("InputCurrent->" + task + "\n" + "InputSum->" + requestList.length)
+        }
+    }, [task]);
+
+    useEffect(() => {
+        if (taskListRef.current) {
+            const nodeCount = taskListRef.current.childElementCount;
+            console.log(`当前有 ${nodeCount} 个dom节点`);
+        }
+    }, [taskList]);
 
     return <>
-        <TodoTaskInput value={task}
-                       onChange={e => setTask(e.target.value)}/>
+        <TodoTaskInput
+            value={task}
+            onChange={e => {
+                setTask(e.target.value)
+            }}/>
 
-        <TodoTaskAddButton onClick={() => addTaskToList(task)}>
+        <TodoTaskAddButton
+            onClick={() => {
+                if (task.trim()) {
+                    setTaskList(prev => [...prev, task]);
+                }
+            }}>
             添加
         </TodoTaskAddButton>
 
-        <TodoTaskList taskList={taskList}/>
+        <div ref={taskListRef}>
+            <TodoTaskList taskList={taskList}/>
+        </div>
     </>
 }
 

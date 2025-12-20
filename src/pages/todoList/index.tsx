fd5808732a936ebaede332d04d4d64969bcc2d71
yuan <yuan0x00@gmail.com>
2025-12-20 19:17:27 +0800

    full screen

--- src/pages/todoList/index.tsx
+++ src/pages/todoList/index.tsx
@@ -1,52 +1,53 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
-import TodoTaskInput from './components/TodoTaskInput'
-import TodoTaskAddButton from './components/TodoTaskAddButton'
-import TodoTaskList from './components/TodoTaskList'
-import { debounce } from '../../utils/debounce.ts'
+import {useCallback, useEffect, useRef, useState} from 'react'
+import TodoTaskInput from './components/TodoTaskInput.tsx'
+import TodoTaskAddButton from './components/TodoTaskAddButton.tsx'
+import TodoTaskList from './components/TodoTaskList.tsx'
+import {debounce} from '../../utils/debounce.ts'
 
 function TodoList() {
-    const [taskList, setTaskList] = useState<string[]>([]);
-    const [requestCount, setRequestCount] = useState<number>(0);
-    const taskRef = useRef<string>('');
-    const taskListRef = useRef<HTMLDivElement>(null);
+    const [taskList, setTaskList] = useState<string[]>([])
+    const [requestCount, setRequestCount] = useState<number>(0)
+    const taskRef = useRef<string>('')
+    const taskListRef = useRef<HTMLDivElement>(null)
 
-    const handleChange = useCallback(debounce((e) => {
-        const task = e.target.value;
-        if (task || task.trim() != '') {
-            taskRef.current = task;
-            setRequestCount(prevCount => prevCount + 1);
-            console.log(`内容更改，发送请求`);
-        }
-    }, 300), [])
+    const handleChange = useCallback(
+        debounce(e => {
+            const task = e.target.value
+            if (task || task.trim() != '') {
+                taskRef.current = task
+                setRequestCount(prevCount => prevCount + 1)
+                console.log(`内容更改，发送请求`)
+            }
+        }, 300),
+        [],
+    )
 
     const handleAdd = () => {
         if (taskRef.current?.trim()) {
-            setTaskList([...taskList, taskRef.current]);
+            setTaskList([...taskList, taskRef.current])
         }
     }
 
     useEffect(() => {
-        console.log(`总共发起了 ${requestCount} 次请求`);
-    }, [requestCount]);
+        console.log(`总共发起了 ${requestCount} 次请求`)
+    }, [requestCount])
 
     useEffect(() => {
         if (taskListRef.current) {
-            const nodeCount = taskListRef.current.childElementCount;
-            console.log(`当前有 ${nodeCount} 个dom节点`);
+            const nodeCount = taskListRef.current.childElementCount
+            console.log(`当前有 ${nodeCount} 个dom节点`)
         }
-    }, [taskList]);
+    }, [taskList])
 
     return (
-        <>
-            <TodoTaskInput handleChange={handleChange}/>
+        <div className={'full-screen'}>
+            <TodoTaskInput handleChange={handleChange} />
 
-            <TodoTaskAddButton handleClick={handleAdd}>
-                添加
-            </TodoTaskAddButton>
+            <TodoTaskAddButton handleClick={handleAdd}>添加</TodoTaskAddButton>
 
-            <TodoTaskList ref={taskListRef} taskList={taskList}/>
-        </>
+            <TodoTaskList ref={taskListRef} taskList={taskList} />
+        </div>
     )
 }
 
-export default TodoList;
+export default TodoList
